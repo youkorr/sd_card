c122d63a8af2751ea1581eac775a19966cfac215
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-03 01:06:00 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -1,15 +1,15 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import binary_sensor
 from esphome.const import CONF_ID, CONF_PLATFORM, CONF_FILES
 
 DEPENDENCIES = []
 CODEOWNERS = ["@votre_nom"]
 
+CONF_STORAGE = "storage"
 storage_ns = cg.esphome_ns.namespace('storage')
 StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
 
-CONFIG_SCHEMA = cv.Schema({
+STORAGE_SCHEMA = cv.Schema({
     cv.Required(CONF_PLATFORM): cv.one_of("flash", "inline", lower=True),
     cv.Required(CONF_FILES): cv.ensure_list({
         cv.Required("source"): cv.string,
@@ -17,11 +17,16 @@ CONFIG_SCHEMA = cv.Schema({
     }),
 }).extend(cv.COMPONENT_SCHEMA)
 
+CONFIG_SCHEMA = cv.All(
+    cv.ensure_list(STORAGE_SCHEMA),
+)
+
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    
-    cg.add(var.set_platform(config[CONF_PLATFORM]))
-    
-    for file in config[CONF_FILES]:
-        cg.add(var.add_file(file["source"], file["id"]))
+    for conf in config:
+        var = cg.new_Pvariable(conf[CONF_ID])
+        yield cg.register_component(var, conf)
+        
+        cg.add(var.set_platform(conf[CONF_PLATFORM]))
+        
+        for file in conf[CONF_FILES]:
+            cg.add(var.add_file(file["source"], file["id"]))
