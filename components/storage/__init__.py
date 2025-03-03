ad413e74f2bc65b368c1bf8dd28a10f177af61ed
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-03 01:01:04 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -1,4 +1,27 @@
-# Interface Python pour le stockage
-from .storage import Storage
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import binary_sensor
+from esphome.const import CONF_ID, CONF_PLATFORM, CONF_FILES
 
-__all__ = ['Storage']
+DEPENDENCIES = []
+CODEOWNERS = ["@votre_nom"]
+
+storage_ns = cg.esphome_ns.namespace('storage')
+StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.Required(CONF_PLATFORM): cv.one_of("flash", "inline", lower=True),
+    cv.Required(CONF_FILES): cv.ensure_list({
+        cv.Required("source"): cv.string,
+        cv.Required("id"): cv.string,
+    }),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    
+    cg.add(var.set_platform(config[CONF_PLATFORM]))
+    
+    for file in config[CONF_FILES]:
+        cg.add(var.add_file(file["source"], file["id"]))
