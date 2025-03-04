0b936224b1e5ad3f5382edc99cc351737858f6e4
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 16:41:29 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_PLATFORM, CONF_FILES
 from esphome import automation
 
-DEPENDENCIES = []
+DEPENDENCIES = ["sd_card"]  # Ajout de la dépendance SD card
 CODEOWNERS = ["@votre_nom"]
 
 CONF_STORAGE = "storage"
@@ -11,6 +11,8 @@ CONF_IMAGES = "images"
 
 storage_ns = cg.esphome_ns.namespace('storage')
 StorageComponent = storage_ns.class_('StorageComponent', cg.Component)
+PlayMediaAction = storage_ns.class_('PlayMediaAction', automation.Action)
+LoadImageAction = storage_ns.class_('LoadImageAction', automation.Action)
 
 # Schéma pour les actions
 STORAGE_PLAY_MEDIA_SCHEMA = cv.Schema({
@@ -25,27 +27,39 @@ STORAGE_LOAD_IMAGE_SCHEMA = cv.Schema({
 
 # Enregistrement des actions
 @automation.register_action(
-    "storage_sd_play.media",
-    storage_ns.class_("PlayMediaAction"),  # Notez le passage du template
+    "storage.play_media",
+    PlayMediaAction,
     STORAGE_PLAY_MEDIA_SCHEMA,
 )
-def storage_play_media_to_code(config, action_id, template_arg, args):
-    storage = yield cg.get_variable(config["storage_id"])
+async def storage_play_media_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
+    storage = await cg.get_variable(config["storage_id"])
     cg.add(var.set_storage(storage))
     cg.add(var.set_media_file(config["media_file"]))
-    yield var
+    return var
+
+@automation.register_action(
+    "storage.load_image",
+    LoadImageAction,
+    STORAGE_LOAD_IMAGE_SCHEMA,
+)
+async def storage_load_image_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    storage = await cg.get_variable(config["storage_id"])
+    cg.add(var.set_storage(storage))
+    cg.add(var.set_image_id(config["image_id"]))
+    return var
 
 # Schéma pour le stockage
 STORAGE_SCHEMA = cv.Schema({
-    cv.Required(CONF_ID): cv.declare_id(StorageComponent),
+    cv.GenerateID(): cv.declare_id(StorageComponent),
     cv.Required(CONF_PLATFORM): cv.one_of("flash", "inline", "sd_card", lower=True),
     cv.Required(CONF_FILES): cv.ensure_list({
-        cv.Required("source"): cv.string,
+        cv.Required("source"): cv.file_,
         cv.Required("id"): cv.string,
     }),
     cv.Optional(CONF_IMAGES): cv.ensure_list({
-        cv.Required("file"): cv.string,
+        cv.Required("file"): cv.file_,
         cv.Required("id"): cv.string,
     }),
 }).extend(cv.COMPONENT_SCHEMA)
@@ -54,10 +68,10 @@ CONFIG_SCHEMA = cv.All(
     cv.ensure_list(STORAGE_SCHEMA),
 )
 
-def to_code(config):
+async def to_code(config):
     for conf in config:
         var = cg.new_Pvariable(conf[CONF_ID])
-        yield cg.register_component(var, conf)
+        await cg.register_component(var, conf)
         
         cg.add(var.set_platform(conf[CONF_PLATFORM]))
         
@@ -67,5 +81,6 @@ def to_code(config):
         if CONF_IMAGES in conf:
             for image in conf[CONF_IMAGES]:
                 cg.add(var.add_image(image["file"], image["id"]))
+
 
 
