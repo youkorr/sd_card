1815b89a367741ca24de3a0af9c7a2bf16749ccd
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-04 23:46:26 +0100

    Update __init__.py

--- components/storage/__init__.py
+++ components/storage/__init__.py
@@ -12,60 +12,41 @@ CONF_DATA = "data"
 storage_ns = cg.esphome_ns.namespace('storage')
 MediaPlayerComponent = storage_ns.class_('MediaPlayerComponent', cg.Component)
 
-# Unique ID validation function
-def validate_unique_media_player_id(full_config):
-    # Track existing IDs across all configurations
-    existing_ids = set()
-    
-    # Check for duplicates in storage configurations
-    for conf in full_config.get('storage', []):
-        conf_id = conf.get('id')
-        if not conf_id:
-            continue
-        
-        if conf_id in existing_ids:
-            raise cv.Invalid(f"Duplicate media player ID: {conf_id}")
-        
-        existing_ids.add(conf_id)
-    
-    return full_config
+# Schema for individual file
+FILE_SCHEMA = cv.Schema({
+    cv.Required(CONF_DATA): cv.returning_lambda,
+    cv.Required(CONF_ID): cv.string,
+})
 
-# Schema for storage configuration
-STORAGE_SCHEMA = cv.Schema({
+# Schema for media player configuration
+MEDIA_PLAYER_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.declare_id(MediaPlayerComponent),
     cv.Required(CONF_PLATFORM): cv.one_of("media_player", lower=True),
-    cv.Optional(CONF_FILES): cv.ensure_list({
-        cv.Required(CONF_DATA): cv.returning_lambda,
-        cv.Required(CONF_ID): cv.string,
-    }),
-}).extend(cv.COMPONENT_SCHEMA)
+    cv.Optional(CONF_FILES, default=[]): cv.ensure_list(FILE_SCHEMA),
+})
 
-# Modify CONFIG_SCHEMA to use the validation function
+# Configuration schema
 CONFIG_SCHEMA = cv.Schema({
-    cv.Optional('storage'): cv.All(
-        cv.ensure_list(STORAGE_SCHEMA),
-        validate_unique_media_player_id
-    )
+    cv.Optional(CONF_STORAGE): cv.ensure_list(MEDIA_PLAYER_SCHEMA)
 })
 
 def to_code(config):
-    for conf in config.get('storage', []):
+    for conf in config.get(CONF_STORAGE, []):
         var = cg.new_Pvariable(conf[CONF_ID])
         yield cg.register_component(var, conf)
         
         cg.add(var.set_platform(conf[CONF_PLATFORM]))
         
-        if CONF_FILES in conf:
-            for file in conf[CONF_FILES]:
-                # Process lambda for data
-                data_lambda = yield cg.process_lambda(
-                    file[CONF_DATA], 
-                    [],  # No arguments
-                    return_type=cg.std_vector(cg.uint8)
-                )
-                
-                # Add file with processed lambda
-                cg.add(var.add_file(data_lambda, file[CONF_ID]))
+        for file in conf.get(CONF_FILES, []):
+            # Process lambda for data
+            data_lambda = yield cg.process_lambda(
+                file[CONF_DATA], 
+                [],  # No arguments
+                return_type=cg.std_vector(cg.uint8)
+            )
+            
+            # Add file with processed lambda
+            cg.add(var.add_file(data_lambda, file[CONF_ID]))
 
 
 
