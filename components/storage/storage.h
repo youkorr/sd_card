1e72c7e1dff6c3a56f18ceee87eb2508557b29a6
youkorr <96953496+youkorr@users.noreply.github.com>
2025-03-06 23:31:13 +0100

    Update storage.h

--- components/storage/storage.h
+++ components/storage/storage.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/core/automation.h"
 #include <vector>
 #include <string>
 
@@ -14,13 +13,15 @@ class StorageComponent : public Component {
   void add_file(const std::string &source, const std::string &id) {
     files_.emplace_back(source, id);
   }
-  void add_image(const std::string &file, const std::string &id) {
-    images_.emplace_back(file, id);
+  std::string get_file_path(const std::string &id) const {
+    for (const auto &file : files_) {
+      if (file.second == id) {
+        return file.first;
+      }
+    }
+    return "";
   }
   void setup() override;
-  
-  // New method for media player integration
-  std::string get_file_path(const std::string &file_id) const;
 
  protected:
   void setup_sd_card();
@@ -30,28 +31,6 @@ class StorageComponent : public Component {
  private:
   std::string platform_;
   std::vector<std::pair<std::string, std::string>> files_;
-  std::vector<std::pair<std::string, std::string>> images_;
-};
-
-template<typename... Ts>
-class PlayMediaAction : public Action<Ts...> {
- public:
-  PlayMediaAction(StorageComponent *storage) : storage_(storage) {}
-  
-  TEMPLATABLE_VALUE(std::string, media_file)
-  TEMPLATABLE_VALUE(bool, announcement)
-  TEMPLATABLE_VALUE(bool, enqueue)
-
-  void play(Ts... x) override {
-    if (storage_) {
-      auto file_path = storage_->get_file_path(this->media_file_.value(x...));
-      // Implementation to play the file would go here
-      // This is where you'd integrate with your media player
-    }
-  }
-
- protected:
-  StorageComponent *storage_;
 };
 
 }  // namespace storage
